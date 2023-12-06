5a7d8b4ef53a10aabb7f7bf52b81dfc8577e6470
bcart01v <38075349+bcart01v@users.noreply.github.com>
2023-12-05 22:25:05 -0600

    Refactor Code

--- executeCommand.c
+++ executeCommand.c
@@ -1,59 +1,55 @@
 #include "header.h"
-
 /**
  * executeCommand - Executes a command with arguments.
  * @command: The command to execute.
  * @args: An array of strings containing the command's arguments
  * @path_copy: A copy of the path environment variable.
-*/
+ */
+
 void executeCommand(__attribute__((unused))char *command, char *args[], char *path_copy)
 {
-    pid_t pid = fork();
-    if (pid == 0) { /* Child process */
-        if (strchr(args[0], '/') != NULL) {
-            /* Execute the command directly if it contains '/' */
-            if (execve(args[0], args, NULL) == -1)
-            {
-                perror("execve");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            char *token = strtok(path_copy, ":");
-            while (token != NULL)
-            {
-                char cmd[1024];
-                snprintf(cmd, sizeof(cmd), "%s/%s", token, args[0]);
-                /*Remove newline, would ruin new file creation */
-                cmd[strcspn(cmd, "\n")] = '\0';
-                if (access(cmd, X_OK) == 0)
-                {
-                    /* Create an array to store the environment variables */
-                    char *envp[] = {NULL};
-                    /* Execute the command with arguments and environment */
-                    if (execve(cmd, args, envp) == -1)
-                    {
-                        perror("execve");
-                        exit(EXIT_FAILURE);
-                        }
-                }
-                token = strtok(NULL, ":");
-            }
-            printf("Command not found in PATH: %s\n", args[0]);
-            exit(EXIT_FAILURE);
-        }
-    }
-    else if (pid > 0)
-    {
-     /* Parent process, wait for the child process */
-        int status;
-        wait(&status);
-    }
-    else
-    {
-        /* Fork failed */
-        perror("fork");
-        exit(EXIT_FAILURE);
-    }
+	pid_t pid = fork();
+	if (pid == 0)
+	{ 
+		/* Child Process right here */
+		if (strchr(args[0], '/') != NULL)
+		{
+			execve(args[0], args, NULL);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
+			char *token, *cmd;
+			char local_path_copy[1024];
+			strncpy(local_path_copy, path_copy, sizeof(local_path_copy));
+			token = strtok(local_path_copy, ":");
+			while (token != NULL)
+			{
+				cmd = malloc(strlen(token) + strlen(args[0]) + 2);
+				sprintf(cmd, "%s/%s", token, args[0]);
+				if (access(cmd, X_OK) == 0)
+				{
+					execve(cmd, args, NULL);
+					perror("execve");
+					exit(EXIT_FAILURE);
+				}
+				free(cmd);
+				token = strtok(NULL, ":");
+			}
+			printf("%s\n", args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else if (pid > 0)
+	{
+		/* This is the parent process */
+		int status;
+		wait(&status);
+	}
+	else
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
 }
--- main.c
+++ main.c
@@ -1,59 +1,74 @@
 #include "header.h"
+
 /**
  * main - Recreation of shell.
  *
  * Return: 0 On success.
-*/
+ */
+
 int main(void)
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
-    char *path = getenv("PATH");
-    char *path_copy = strdup(path);
-    char *args[10];
-    int i;
+	char *buffer = NULL;
+	size_t bufsize = 0;
+	ssize_t lineSize;
+	char *path = getenv("PATH");
+	char *path_copy = strdup(path);
+	char *args[10];
+	int i;
 
-    while (1)
-    {
-	if (isatty(STDIN_FILENO))
-		printf("Enter a command\n");
+	while (1) {
+		if (isatty(STDIN_FILENO))
+		{
+			printf("Enter a command\n");
+		}
 
-	getline(&buffer, &bufsize, stdin);
+		lineSize = getline(&buffer, &bufsize, stdin);
+		if (lineSize == -1) {
+			if (feof(stdin)) {
+			/* End of file (Or input?) Which stops the while loop */
+				break;
+			}
+			else if (ferror(stdin))
+			{
+				/* This is an error. What should the error message be?*/
+				perror("getline");
+				break;
+			}
+		}
 
-        /* Remove the trailing newline character */
-        buffer[strcspn(buffer, "\n")] = '\0';
-        /* If we don't set i to 0, it will only count up every time */
-        /* the program is run and stop at 9 */
-        i = 0;
-        args[i] = strtok(buffer, " ");
-        while (args[i] != NULL && i < 9)
-        {
-            i++;
-            args[i] = strtok(NULL, " ");
-        }
-        /* Make sure the last element is NULL */
-        args[i] = NULL;
-        if (strcmp(args[0], "copy") == 0) {
-            if (args[1] != NULL && args[2] != NULL)
-            {
-            /* Copy the file */
-                copyFile(args[1], args[2]);
-            }
-            else
-            {
-                /* debugging. failure message, probably change this? */
-                printf("Usage: copy source_file destination_file\n");
-            }
+		/* Remove the newline */
+		buffer[strcspn(buffer, "\n")] = '\0';
+		i = 0;
+		args[i] = strtok(buffer, " ");
+		while (args[i] != NULL && i < 9)
+		{
+			i++;
+			args[i] = strtok(NULL, " ");
+		}
+		args[i] = NULL;
+
+		if (args[0] != NULL)
+		{
+			if (strcmp(args[0], "copy") == 0)
+			{
+				if (args[1] != NULL && args[2] != NULL)
+				{
+					copyFile(args[1], args[2]);
+				}
+				else
+				{
+					/*This is an error */
+					printf("Usage: copy source_file destination_file\n");
+				}
+			}
+			else
+			{
+				executeCommand(args[0], args, path_copy);
+			}
+		}
 	}
-        else
-        {
-            /* Execute the command */
-            executeCommand(args[0], args, path_copy);
-        }
-        free(path_copy);
-        path_copy = strdup(path);
-    }
-    free(buffer);
-    free(path_copy);
-    return (0);
+
+	free(buffer);
+	free(path_copy);
+	return 0;
 }
